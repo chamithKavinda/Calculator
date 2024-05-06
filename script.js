4e41981dff918a474192c4cea292192d24e150e7
chamithKavinda <chamith13kavinda@gmail.com>
2024-05-06 10:11:25 +0530

    update

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
 const toggleIcon = document.querySelector(".toggler-icon");
 let isDark = true;
+
 themeToggleBtn.onclick = () => {
   calculator.classList.toggle("dark");
   themeToggleBtn.classList.toggle("active");
